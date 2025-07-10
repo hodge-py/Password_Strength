40db2c0ee818e9472dddd40e8069680e79b9e63e
hodge-py <105604814+hodge-py@users.noreply.github.com>
2025-07-10 13:05:15 -0600

    c

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import numpy as np
 from flask import Flask, render_template
 from flask import request
 import pandas as pd
+import pickle
 app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
@@ -9,14 +11,25 @@ def hello_world():
 
 @app.route('/output', methods=['POST'])
 def output():
+    with open('./data/rockyou-75.txt', 'r') as f:
+        common_passwords = [line.strip().lower() for line in f if line.strip()]
+    def check_common_substrings(password):
+        password = password.lower()
+        return any(pw for pw in common_passwords if pw in password)
     special_characters = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
     projectpath = request.form['projectFilepath']
-    print(projectpath)
     lens = len(projectpath)
     upper = any(char.isupper() for char in projectpath)
     lower = any(char.islower() for char in projectpath)
     special = any(char in special_characters for char in projectpath)
     number = any(char.isnumeric() for char in projectpath)
-    tmpdf = pd.DataFrame(data={'password_length': [lens], 'Upper': [upper], 'Lower': [lower], 'Special': [special], 'Number': [number]})
-    print(tmpdf)
-    return render_template('output.html',password=projectpath)
+    newArr = [[check_common_substrings(projectpath), lens, upper, lower, special, number]]
+    loaded_model = pickle.load(open('final_model.sav', 'rb'))
+    prediction = loaded_model.predict(newArr)
+    if prediction == 0:
+        prediction = 'Weak'
+    elif prediction == 1:
+        prediction = 'Medium'
+    else:
+        prediction = 'Strong'
+    return render_template('output.html',password=prediction, passName=projectpath)
--- passStrength.py
+++ passStrength.py
@@ -6,24 +6,42 @@ import sklearn as sk
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.ensemble import HistGradientBoostingClassifier
+from scipy.sparse import hstack
+from scipy.sparse import csr_matrix
+import pickle
 
 pd.options.display.max_columns = None
 
-df = pd.read_csv('./data/Passwords.csv')
+df = pd.read_csv('./data/new_passwords.csv')
+"""
+with open('./data/rockyou-75.txt', 'r') as f:
+    common_passwords = [line.strip().lower() for line in f if line.strip()]
+
+# Function to check for common passwords as substrings
+def check_common_substrings(password):
+    password = password.lower()
+    return any(pw for pw in common_passwords if pw in password)
+
+df['has_common_word'] = df['password'].apply(lambda x: check_common_substrings(x))
+
+df.to_csv('./data/new_passwords.csv', index=False)
+"""
 
 special_characters =  " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
 
 df['password_length'] = df['password'].apply(lambda x: len(x))
 
-df['Upper'] = df['password'].apply(lambda x: any(char.isupper() for char in x))
+df['upper'] = df['password'].apply(lambda x: any(char.isupper() for char in x))
 
-df['Lower'] = df['password'].apply(lambda x: any(char.islower() for char in x))
+df['lower'] = df['password'].apply(lambda x: any(char.islower() for char in x))
 
-df['Special'] = df['password'].apply(lambda x: any(char in special_characters for char in x))
+df['special'] = df['password'].apply(lambda x: any(char in special_characters for char in x))
 
-df['Number'] = df['password'].apply(lambda x: any(char.isnumeric() for char in x))
+df['number'] = df['password'].apply(lambda x: any(char.isnumeric() for char in x))
 
-X = df[['password_length','Upper','Lower','Special','Number']]
+X = df[['password_length','upper','lower','special','number','has_common_word']]
 
 y = df['strength']
 
@@ -32,14 +50,17 @@ print(df.head(10))
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.33, random_state=42)
 
-clf = GradientBoostingClassifier(n_estimators=100, learning_rate=1.0, max_depth=1, random_state=0).fit(X_train, y_train)
+clf = GradientBoostingClassifier(learning_rate=1.0, max_depth=1, random_state=0).fit(X_train, y_train)
 
 preds = clf.predict(X_test)
 
 print(classification_report(y_test, preds))
-tmpdf = pd.DataFrame(data={'password_length': [1], 'Upper': [True], 'Lower': [False], 'Special': [False],'Number': [False]})
-print(tmpdf)
-print(clf.predict(tmpdf))
+
+filename = 'final_model.sav'
+pickle.dump(clf, open(filename, 'wb'))
+#tmpdf = pd.DataFrame(data={'password_length': [1], 'upper': [True], 'lower': [False], 'special': [False],'number': [False]})
+#print(tmpdf)
+#print(clf.predict(tmpdf))
 
 
 
